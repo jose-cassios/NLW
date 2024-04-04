de39e2d59304a37b6c347a9458550ff9113db103
Jose Cassios <cassios.torres.010@gmail.com>
2024-04-03 22:33:17 -0300

    feat: Implementing events_repository and tests

--- src/models/settings/connection.py
+++ src/models/settings/connection.py
@@ -1,22 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-class DBConnectionHandler:
+class __DBConnectionHandler:
     def __init__(self) -> None:
-        self.__connection_string = f"{"sqlite"}:///{"storage.db"}"
+        self.__connection_string = "{}:///{}".format(
+            "sqlite",
+            "storage.db"
+        )
         self.__engine = None
         
     def connect_to_db(self) -> None:
         self.__engine = create_engine(self.__connection_string)
-        self.__session = None
+        self.session = None
 
     def get_engine(self):
         return self.__engine
     
     def __enter__(self):
         session_maker = sessionmaker()
-        self.__session = session_maker(bind=self.__engine)
+        self.session = session_maker(bind=self.__engine)
         return self
         
-    def __exit__(self):
-        self.__session.close()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
+
+db_connection_handler = __DBConnectionHandler()
